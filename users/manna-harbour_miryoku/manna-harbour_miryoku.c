6b2750629c9f4794f578e7c7b3cc24e78560a000
Chester Luna <3890275+chluna@users.noreply.github.com>
2024-01-16 02:10:58 +0800

    Fix bug on key override with shift on KC_DOT and KC_SLSH

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -150,6 +150,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case LAUNCH:
                 invoke_app_launcher();
                 break;
+            case ALGR_T(KC_DOT):
+                if (mods & MOD_MASK_SHIFT) {
+                    unregister_mods(mods);
+                    tap_code16(KC_COLN);
+                    register_mods(mods);
+                    return false;
+                }
+                break;
+            case LT(U_BUTTON, KC_SLSH):
+                if (mods & MOD_MASK_SHIFT) {
+                    unregister_mods(mods);
+                    tap_code16(KC_BSLS);
+                    register_mods(mods);
+                    return false;
+                }
+                break;
         }
     }
 
@@ -177,16 +193,12 @@ MIRYOKU_LAYER_LIST
 
 // Shift functions
 
-const key_override_t comma_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_COLN);
-const key_override_t dot_key_override   = ko_make_basic(MOD_MASK_SHIFT, KC_DOT,  KC_SCLN);
-const key_override_t slash_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, KC_BSLS);
+const key_override_t comma_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_SCLN);
 const key_override_t volup_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_VOLU, KC_BRIU);
 const key_override_t voldn_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_VOLD, KC_BRID);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &comma_key_override,
-    &dot_key_override,
-    &slash_key_override,
     &volup_key_override,
     &voldn_key_override,
     NULL
